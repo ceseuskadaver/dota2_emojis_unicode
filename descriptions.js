7d1142a9064d1d4b81a6a1b06efe2f1ba1ed3afa
s3rbug <bugaychuk.sergiy@gmail.com>
2023-05-03 15:31:31 +0300

    descriptions in separate file

---
+++ descriptions.js
@@ -0,0 +1,53 @@
+export const DESCRIPTIONS = [
+  {
+    code: 270,
+    description: ":davion_sexy:"
+  },
+  {
+    code: 340,
+    description: ":eyebrow:"
+  },
+  {
+    code: 2000,
+    description: ":uncalibrated:"
+  },
+  {
+    code: 2001,
+    description: ":herald:"
+  },
+  {
+    code: 2002,
+    description: ":guardian:"
+  },
+  {
+    code: 2003,
+    description: ":crusader:"
+  },
+  {
+    code: 2004,
+    description: ":archon:"
+  },
+  {
+    code: 2005,
+    description: ":legend:"
+  },
+  {
+    code: 2006,
+    description: ":ancient:"
+  },
+  {
+    code: 2007,
+    description: ":divine:"
+  },
+  {
+    code: 2008,
+    description: ":immortal:"
+  },
+  {
+    code: 2009,
+    description: ":top100:"
+  },
+  {
+    code: 2010,
+    description: ":top1:"
+  }]
--- unicode.js
+++ unicode.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+import { writeFile } from 'fs';
+import {DESCRIPTIONS} from './descriptions'
 
 const emojiDescriptionMap = new Map()
 
@@ -15,46 +16,7 @@ function configureDescriptions(descriptions) {
   }
 }
 
-configureDescriptions([{
-  code: 270,
-  description: ":davion_sexy:"
-}, {
-  code: 340,
-  description: ":eyebrow:"
-}, {
-  code: 2000,
-  description: ":uncalibrated:"
-}, {
-  code: 2001,
-  description: ":herald:"
-}, {
-  code: 2002,
-  description: ":guardian:"
-}, {
-  code: 2003,
-  description: ":crusader:"
-}, {
-  code: 2004,
-  description: ":archon:"
-}, {
-  code: 2005,
-  description: ":legend:"
-}, {
-  code: 2006,
-  description: ":ancient:"
-}, {
-  code: 2007,
-  description: ":divine:"
-}, {
-  code: 2008,
-  description: ":immortal:"
-}, {
-  code: 2009,
-  description: ":top100:"
-}, {
-  code: 2010,
-  description: ":top1:"
-}])
+configureDescriptions(DESCRIPTIONS)
 
 let output = ""
 
@@ -68,7 +30,7 @@ for (let i = 1; i <= 6399; ++i) {
   output += `${i} (${unicode}): bind <your_key> "say ${unescape(unicode)}"${description}\n`
 }
 
-fs.writeFile("emojis.txt", output, { flag: "w+" }, (err) => {
+writeFile("emojis.txt", output, { flag: "w+" }, (err) => {
   if (err) {
     console.log(err);
   }
