c21da080e29f32d4af118bc7353c88464ecfb170
s3rbug <bugaychuk.sergiy@gmail.com>
2023-05-05 17:14:03 +0300

    added emojis description from pak01_dir.vpk file

--- descriptions.js
+++ descriptions.js
@@ -1,54 +1,1349 @@
-export const DESCRIPTIONS = [
-  {
-    code: 270,
-    description: ":davion_sexy:"
-  },
-  {
-    code: 340,
-    description: ":eyebrow:"
-  },
-  {
-    code: 2000,
-    description: ":uncalibrated:"
-  },
-  {
-    code: 2001,
-    description: ":herald:"
-  },
-  {
-    code: 2002,
-    description: ":guardian:"
-  },
-  {
-    code: 2003,
-    description: ":crusader:"
-  },
-  {
-    code: 2004,
-    description: ":archon:"
-  },
-  {
-    code: 2005,
-    description: ":legend:"
-  },
-  {
-    code: 2006,
-    description: ":ancient:"
-  },
-  {
-    code: 2007,
-    description: ":divine:"
-  },
-  {
-    code: 2008,
-    description: ":immortal:"
-  },
-  {
-    code: 2009,
-    description: ":top100:"
-  },
-  {
-    code: 2010,
-    description: ":top1:"
-  }
+export const DESCRIPTIONS = [{
+  code: 1,
+  description: ":wink:"
+},
+{
+  code: 2,
+  description: ":blush:"
+},
+{
+  code: 3,
+  description: ":cheeky:"
+},
+{
+  code: 4,
+  description: ":cool:"
+},
+{
+  code: 5,
+  description: ":crazy:"
+},
+{
+  code: 6,
+  description: ":cry:"
+},
+{
+  code: 7,
+  description: ":disapprove:"
+},
+{
+  code: 8,
+  description: ":doubledamage:"
+},
+{
+  code: 9,
+  description: ":facepalm:"
+},
+{
+  code: 10,
+  description: ":happytears:"
+},
+{
+  code: 11,
+  description: ":haste:"
+},
+{
+  code: 12,
+  description: ":hex:"
+},
+{
+  code: 13,
+  description: ":highfive:"
+},
+{
+  code: 14,
+  description: ":huh:"
+},
+{
+  code: 15,
+  description: ":hush:"
+},
+{
+  code: 16,
+  description: ":illusion:"
+},
+{
+  code: 17,
+  description: ":invisibility:"
+},
+{
+  code: 18,
+  description: ":laugh:"
+},
+{
+  code: 19,
+  description: ":rage:"
+},
+{
+  code: 20,
+  description: ":regeneration:"
+},
+{
+  code: 21,
+  description: ":sad:"
+},
+{
+  code: 22,
+  description: ":sick:"
+},
+{
+  code: 23,
+  description: ":sleeping:"
+},
+{
+  code: 24,
+  description: ":smile:"
+},
+{
+  code: 25,
+  description: ":surprise:"
+},
+{
+  code: 26,
+  description: ":aaaah:"
+},
+{
+  code: 27,
+  description: ":burn:"
+},
+{
+  code: 28,
+  description: ":hide:"
+},
+{
+  code: 29,
+  description: ":iceburn:"
+},
+{
+  code: 30,
+  description: ":tears:"
+},
+{
+  code: 31,
+  description: ":techies:"
+},
+{
+  code: 32,
+  description: ":fail:"
+},
+{
+  code: 33,
+  description: ":goodjob:"
+},
+{
+  code: 34,
+  description: ":headshot:"
+},
+{
+  code: 35,
+  description: ":heart:"
+},
+{
+  code: 36,
+  description: ":horse:"
+},
+{
+  code: 37,
+  description: ":grave:"
+},
+{
+  code: 38,
+  description: ":ti4copper:"
+},
+{
+  code: 39,
+  description: ":ti4bronze:"
+},
+{
+  code: 40,
+  description: ":ti4silver:"
+},
+{
+  code: 41,
+  description: ":ti4gold:"
+},
+{
+  code: 42,
+  description: ":ti4platinum:"
+},
+{
+  code: 43,
+  description: ":ti4diamond:"
+},
+{
+  code: 44,
+  description: ":dac15_blush:"
+},
+{
+  code: 45,
+  description: ":dac15_surprise:"
+},
+{
+  code: 46,
+  description: ":dac15_cool:"
+},
+{
+  code: 47,
+  description: ":dac15_duel:"
+},
+{
+  code: 48,
+  description: ":dac15_frog:"
+},
+{
+  code: 49,
+  description: ":dac15_face:"
+},
+{
+  code: 50,
+  description: ":dac15_nosewipe:"
+},
+{
+  code: 51,
+  description: ":dac15_stab:"
+},
+{
+  code: 52,
+  description: ":dac15_transform:"
+},
+{
+  code: 53,
+  description: ":pup:"
+},
+{
+  code: 54,
+  description: ":bts_bristle:"
+},
+{
+  code: 55,
+  description: ":bts_godz:"
+},
+{
+  code: 56,
+  description: ":bts_lina:"
+},
+{
+  code: 57,
+  description: ":bts_merlini:"
+},
+{
+  code: 58,
+  description: ":bts_rosh:"
+},
+{
+  code: 59,
+  description: ":cocky:"
+},
+{
+  code: 60,
+  description: ":devil:"
+},
+{
+  code: 61,
+  description: ":happy:"
+},
+{
+  code: 62,
+  description: ":thinking:"
+},
+{
+  code: 63,
+  description: ":tp:"
+},
+{
+  code: 64,
+  description: ":salty:"
+},
+{
+  code: 65,
+  description: ":angel:"
+},
+{
+  code: 66,
+  description: ":blink:"
+},
+{
+  code: 67,
+  description: ":snot:"
+},
+{
+  code: 68,
+  description: ":stunned:"
+},
+{
+  code: 69,
+  description: ":disappear:"
+},
+{
+  code: 70,
+  description: ":fire:"
+},
+{
+  code: 71,
+  description: ":bountyrune:"
+},
+{
+  code: 72,
+  description: ":troll:"
+},
+{
+  code: 73,
+  description: ":gross:"
+},
+{
+  code: 74,
+  description: ":ggdire:"
+},
+{
+  code: 75,
+  description: ":ggradiant:"
+},
+{
+  code: 76,
+  description: ":yolo:"
+},
+{
+  code: 77,
+  description: ":throwgame:"
+},
+{
+  code: 78,
+  description: ":aegis2015:"
+},
+{
+  code: 79,
+  description: ":eyeroll:"
+},
+{
+  code: 80,
+  description: ":charm_blush:"
+},
+{
+  code: 81,
+  description: ":charm_cheeky:"
+},
+{
+  code: 82,
+  description: ":charm_cool:"
+},
+{
+  code: 83,
+  description: ":charm_crazy:"
+},
+{
+  code: 84,
+  description: ":charm_cry:"
+},
+{
+  code: 85,
+  description: ":charm_disapprove:"
+},
+{
+  code: 86,
+  description: ":charm_facepalm:"
+},
+{
+  code: 87,
+  description: ":charm_happytears:"
+},
+{
+  code: 88,
+  description: ":charm_highfive:"
+},
+{
+  code: 89,
+  description: ":charm_huh:"
+},
+{
+  code: 90,
+  description: ":charm_hush:"
+},
+{
+  code: 91,
+  description: ":charm_laugh:"
+},
+{
+  code: 92,
+  description: ":charm_rage:"
+},
+{
+  code: 93,
+  description: ":charm_sad:"
+},
+{
+  code: 94,
+  description: ":charm_sick:"
+},
+{
+  code: 95,
+  description: ":charm_sleeping:"
+},
+{
+  code: 96,
+  description: ":charm_surprise:"
+},
+{
+  code: 97,
+  description: ":charm_wink:"
+},
+{
+  code: 98,
+  description: ":charm_smile:"
+},
+{
+  code: 99,
+  description: ":charm_onlooker:"
+},
+{
+  code: 100,
+  description: ":eaglesong_2015:"
+},
+{
+  code: 101,
+  description: ":wrath:"
+},
+{
+  code: 102,
+  description: ":snowman:"
+},
+{
+  code: 103,
+  description: ":healed:"
+},
+{
+  code: 104,
+  description: ":drunk:"
+},
+{
+  code: 105,
+  description: ":trophy_2016:"
+},
+{
+  code: 106,
+  description: ":pa_kiss:"
+},
+{
+  code: 107,
+  description: ":arcane_rune:"
+},
+{
+  code: 108,
+  description: ":fuming:"
+},
+{
+  code: 109,
+  description: ":axe_laugh:"
+},
+{
+  code: 110,
+  description: ":dizzy:"
+},
+{
+  code: 111,
+  description: ":zipper:"
+},
+{
+  code: 112,
+  description: ":blush_smile:"
+},
+{
+  code: 113,
+  description: ":nervous:"
+},
+{
+  code: 114,
+  description: ":surprise_blush:"
+},
+{
+  code: 115,
+  description: ":luna_love:"
+},
+{
+  code: 116,
+  description: ":dead_eyes:"
+},
+{
+  code: 117,
+  description: ":giff:"
+},
+{
+  code: 118,
+  description: ":thumbs_up:"
+},
+{
+  code: 119,
+  description: ":thumbs_down:"
+},
+{
+  code: 120,
+  description: ":money:"
+},
+{
+  code: 121,
+  description: ":unicorn:"
+},
+{
+  code: 122,
+  description: ":naga_song:"
+},
+{
+  code: 123,
+  description: ":poop:"
+},
+{
+  code: 124,
+  description: ":aegis_2016:"
+},
+{
+  code: 125,
+  description: ":tear:"
+},
+{
+  code: 126,
+  description: ":reaver_2016:"
+},
+{
+  code: 127,
+  description: ":bc_100:"
+},
+{
+  code: 128,
+  description: ":bc_fire:"
+},
+{
+  code: 129,
+  description: ":bc_ok:"
+},
+{
+  code: 130,
+  description: ":bc_check:"
+},
+{
+  code: 131,
+  description: ":bc_eyes:"
+},
+{
+  code: 132,
+  description: ":bc_frog:"
+},
+{
+  code: 133,
+  description: ":bc_flex:"
+},
+{
+  code: 134,
+  description: ":cocky_ti6_charm:"
+},
+{
+  code: 135,
+  description: ":devil_ti6_charm:"
+},
+{
+  code: 136,
+  description: ":happy_ti6_charm:"
+},
+{
+  code: 137,
+  description: ":thinking_ti6_charm:"
+},
+{
+  code: 138,
+  description: ":tp_ti6_charm:"
+},
+{
+  code: 139,
+  description: ":angel_ti6_charm:"
+},
+{
+  code: 140,
+  description: ":hex_ti6_charm:"
+},
+{
+  code: 141,
+  description: ":snot_ti6_charm:"
+},
+{
+  code: 142,
+  description: ":stunned_ti6_charm:"
+},
+{
+  code: 143,
+  description: ":disappear_ti6_charm:"
+},
+{
+  code: 144,
+  description: ":fire_ti6_charm:"
+},
+{
+  code: 145,
+  description: ":gross_ti6_charm:"
+},
+{
+  code: 146,
+  description: ":yolo_ti6_charm:"
+},
+{
+  code: 147,
+  description: ":throwgame_ti6_charm:"
+},
+{
+  code: 148,
+  description: ":eyeroll_ti6_charm:"
+},
+{
+  code: 149,
+  description: ":lifestealer_ti6_charm:"
+},
+{
+  code: 150,
+  description: ":heal_ti6_charm:"
+},
+{
+  code: 151,
+  description: ":drunk_ti6_charm:"
+},
+{
+  code: 152,
+  description: ":salty_ti6_charm:"
+},
+{
+  code: 153,
+  description: ":chicken_ti6_charm:"
+},
+{
+  code: 154,
+  description: ":legion_commander_t16_charm:"
+},
+{
+  code: 155,
+  description: ":underlord_ti6_charm:"
+},
+{
+  code: 156,
+  description: ":monkey_king_ti6_charm:"
+},
+{
+  code: 157,
+  description: ":fall_2016_trophy:"
+},
+{
+  code: 158,
+  description: ":jugg:"
+},
+{
+  code: 159,
+  description: ":no:"
+},
+{
+  code: 160,
+  description: ":recharge:"
+},
+{
+  code: 161,
+  description: ":dunno:"
+},
+{
+  code: 162,
+  description: ":exclamation:"
+},
+{
+  code: 163,
+  description: ":venom:"
+},
+{
+  code: 164,
+  description: ":brood_love:"
+},
+{
+  code: 165,
+  description: ":darkmoon_chicken:"
+},
+{
+  code: 166,
+  description: ":bladeform_legacy:"
+},
+{
+  code: 167,
+  description: ":grimace:"
+},
+{
+  code: 168,
+  description: ":aegis_2017:"
+},
+{
+  code: 169,
+  description: ":gem:"
+},
+{
+  code: 170,
+  description: ":plant:"
+},
+{
+  code: 171,
+  description: ":relieved:"
+},
+{
+  code: 172,
+  description: ":foggy:"
+},
+{
+  code: 173,
+  description: ":eh:"
+},
+{
+  code: 174,
+  description: ":flex:"
+},
+{
+  code: 175,
+  description: ":gg:"
+},
+{
+  code: 176,
+  description: ":donkey:"
+},
+{
+  code: 177,
+  description: ":kiss2:"
+},
+{
+  code: 178,
+  description: ":observer_ward:"
+},
+{
+  code: 179,
+  description: ":lick:"
+},
+{
+  code: 180,
+  description: ":laugh2:"
+},
+{
+  code: 181,
+  description: ":sentry_ward:"
+},
+{
+  code: 182,
+  description: ":whew:"
+},
+{
+  code: 183,
+  description: ":gyro:"
+},
+{
+  code: 184,
+  description: ":nerd:"
+},
+{
+  code: 185,
+  description: ":confounded:"
+},
+{
+  code: 186,
+  description: ":joke:"
+},
+{
+  code: 187,
+  description: ":siltbreaker:"
+},
+{
+  code: 188,
+  description: ":mischief:"
+},
+{
+  code: 189,
+  description: ":heykid:"
+},
+{
+  code: 190,
+  description: ":huff:"
+},
+{
+  code: 191,
+  description: ":beg:"
+},
+{
+  code: 192,
+  description: ":kisskiss:"
+},
+{
+  code: 193,
+  description: ":snort:"
+},
+{
+  code: 194,
+  description: ":sweat:"
+},
+{
+  code: 195,
+  description: ":chuckle:"
+},
+{
+  code: 196,
+  description: ":stars:"
+},
+{
+  code: 198,
+  description: ":sleep:"
+},
+{
+  code: 199,
+  description: ":bawl:"
+},
+{
+  code: 200,
+  description: ":siltbreaker2:"
+},
+{
+  code: 201,
+  description: ":brow:"
+},
+{
+  code: 202,
+  description: ":dealwithit:"
+},
+{
+  code: 203,
+  description: ":four:"
+},
+{
+  code: 204,
+  description: ":ggzeus:"
+},
+{
+  code: 205,
+  description: ":hahano:"
+},
+{
+  code: 206,
+  description: ":nuh_uh:"
+},
+{
+  code: 207,
+  description: ":snicker:"
+},
+{
+  code: 208,
+  description: ":welluhh:"
+},
+{
+  code: 209,
+  description: ":wplina:"
+},
+{
+  code: 210,
+  description: ":pudgesteal:"
+},
+{
+  code: 211,
+  description: ":pudgestop:"
+},
+{
+  code: 212,
+  description: ":aegis_2018:"
+},
+{
+  code: 213,
+  description: ":under_win:"
+},
+{
+  code: 214,
+  description: ":under_champ:"
+},
+{
+  code: 215,
+  description: ":under_king:"
+},
+{
+  code: 216,
+  description: ":frostivus_2018:"
+},
+{
+  code: 217,
+  description: ":magus_cypher:"
+},
+{
+  code: 218,
+  description: ":aegis_2019:"
+},
+{
+  code: 219,
+  description: ":morokai_woof:"
+},
+{
+  code: 220,
+  description: ":cny_rat:"
+},
+{
+  code: 221,
+  description: ":doot:"
+},
+{
+  code: 222,
+  description: ":eyebrows:"
+},
+{
+  code: 223,
+  description: ":glare:"
+},
+{
+  code: 224,
+  description: ":grindteeth:"
+},
+{
+  code: 225,
+  description: ":midas:"
+},
+{
+  code: 226,
+  description: ":puckchamp:"
+},
+{
+  code: 227,
+  description: ":skynervous:"
+},
+{
+  code: 228,
+  description: ":tide_drink:"
+},
+{
+  code: 229,
+  description: ":oracleburn:"
+},
+{
+  code: 230,
+  description: ":mortimer_tongue:"
+},
+{
+  code: 231,
+  description: ":iostress:"
+},
+{
+  code: 232,
+  description: ":leisure_lion:"
+},
+{
+  code: 233,
+  description: ":creepdance:"
+},
+{
+  code: 234,
+  description: ":cm_wig:"
+},
+{
+  code: 235,
+  description: ":techywheeze:"
+},
+{
+  code: 236,
+  description: ":icefrog_giggle:"
+},
+{
+  code: 237,
+  description: ":high_five:"
+},
+{
+  code: 238,
+  description: ":guildbanner:"
+},
+{
+  code: 239,
+  description: ":dazzle_shrug:"
+},
+{
+  code: 240,
+  description: ":aegis_2020:"
+},
+{
+  code: 241,
+  description: ":scepter:"
+},
+{
+  code: 242,
+  description: ":big_brain:"
+},
+{
+  code: 243,
+  description: ":chew:"
+},
+{
+  code: 244,
+  description: ":creep_complain:"
+},
+{
+  code: 245,
+  description: ":doom_rip:"
+},
+{
+  code: 246,
+  description: ":giff_diretide:"
+},
+{
+  code: 247,
+  description: ":in_the_bag:"
+},
+{
+  code: 248,
+  description: ":lucky_cat:"
+},
+{
+  code: 249,
+  description: ":pugna_sip:"
+},
+{
+  code: 250,
+  description: ":unlucky_cat:"
+},
+{
+  code: 251,
+  description: ":pump_mad:"
+},
+{
+  code: 252,
+  description: ":pump_sad:"
+},
+{
+  code: 253,
+  description: ":pump_glad:"
+},
+{
+  code: 254,
+  description: ":omgomni:"
+},
+{
+  code: 255,
+  description: ":diredance:"
+},
+{
+  code: 256,
+  description: ":swiz:"
+},
+{
+  code: 257,
+  description: ":wiz_wow:"
+},
+{
+  code: 258,
+  description: ":wiz_bringit:"
+},
+{
+  code: 259,
+  description: ":wiz_glhf:"
+},
+{
+  code: 260,
+  description: ":wiz_help:"
+},
+{
+  code: 261,
+  description: ":wiz_wat:"
+},
+{
+  code: 262,
+  description: ":water_rune:"
+},
+{
+  code: 263,
+  description: ":mango:"
+},
+{
+  code: 264,
+  description: ":earth_perfect:"
+},
+{
+  code: 265,
+  description: ":earth_grin:"
+},
+{
+  code: 266,
+  description: ":abaddon_depressed:"
+},
+{
+  code: 267,
+  description: ":bkb:"
+},
+{
+  code: 268,
+  description: ":bloodthirsty:"
+},
+{
+  code: 269,
+  description: ":dagon:"
+},
+{
+  code: 270,
+  description: ":davion_sexy:"
+},
+{
+  code: 271,
+  description: ":ember_angry:"
+},
+{
+  code: 272,
+  description: ":ember_shakefist:"
+},
+{
+  code: 273,
+  description: ":euls:"
+},
+{
+  code: 274,
+  description: ":fasta:"
+},
+{
+  code: 275,
+  description: ":hoodwink_sad:"
+},
+{
+  code: 276,
+  description: ":hookless:"
+},
+{
+  code: 277,
+  description: ":invoker_dry:"
+},
+{
+  code: 278,
+  description: ":luna_rage:"
+},
+{
+  code: 279,
+  description: ":manta:"
+},
+{
+  code: 280,
+  description: ":marci_whistle:"
+},
+{
+  code: 281,
+  description: ":mirana_drink:"
+},
+{
+  code: 282,
+  description: ":profit:"
+},
+{
+  code: 283,
+  description: ":radiance:"
+},
+{
+  code: 284,
+  description: ":rapier:"
+},
+{
+  code: 286,
+  description: ":shadowdemon_sus:"
+},
+{
+  code: 287,
+  description: ":slardar_sleepy:"
+},
+{
+  code: 288,
+  description: ":spin:"
+},
+{
+  code: 289,
+  description: ":spotted:"
+},
+{
+  code: 290,
+  description: ":storm_clap:"
+},
+{
+  code: 292,
+  description: ":tango:"
+},
+{
+  code: 293,
+  description: ":tb_laugh:"
+},
+{
+  code: 296,
+  description: ":whoahahah:"
+},
+{
+  code: 297,
+  description: ":ti10_compendium:"
+},
+{
+  code: 298,
+  description: ":aghawooga:"
+},
+{
+  code: 299,
+  description: ":awwghanim:"
+},
+{
+  code: 300,
+  description: ":fraghanim:"
+},
+{
+  code: 301,
+  description: ":poghanim:"
+},
+{
+  code: 302,
+  description: ":smughanim:"
+},
+{
+  code: 303,
+  description: ":swaghanim:"
+},
+{
+  code: 305,
+  description: ":come_here:"
+},
+{
+  code: 315,
+  description: ":giggle:"
+},
+{
+  code: 316,
+  description: ":hot_cocoa:"
+},
+{
+  code: 318,
+  description: ":marci_omnom:"
+},
+{
+  code: 319,
+  description: ":not_like_this:"
+},
+{
+  code: 321,
+  description: ":smoked_ya:"
+},
+{
+  code: 323,
+  description: ":power_runes:"
+},
+{
+  code: 324,
+  description: ":bucket:"
+},
+{
+  code: 325,
+  description: ":itsatrap:"
+},
+{
+  code: 326,
+  description: ":itsfromprimal:"
+},
+{
+  code: 327,
+  description: ":flamescepter:"
+},
+{
+  code: 328,
+  description: ":apexmage:"
+},
+{
+  code: 329,
+  description: ":sagan_sad:"
+},
+{
+  code: 330,
+  description: ":flaming_mad:"
+},
+{
+  code: 331,
+  description: ":shut_up:"
+},
+{
+  code: 332,
+  description: ":wait_for_it:"
+},
+{
+  code: 333,
+  description: ":surprised_meepo:"
+},
+{
+  code: 334,
+  description: ":stupid_bruiser:"
+},
+{
+  code: 335,
+  description: ":snowballing:"
+},
+{
+  code: 336,
+  description: ":smugstroke:"
+},
+{
+  code: 337,
+  description: ":shivers:"
+},
+{
+  code: 338,
+  description: ":hungry_hellbear:"
+},
+{
+  code: 339,
+  description: ":furious:"
+},
+{
+  code: 340,
+  description: ":eyebrow:"
+},
+{
+  code: 341,
+  description: ":closecall:"
+},
+{
+  code: 342,
+  description: ":aegis_2022:"
+},
+{
+  code: 343,
+  description: ":shield_rune:"
+},
+{
+  code: 344,
+  description: ":xp_rune:"
+},
+{
+  code: 2000,
+  description: ":uncalibrated:"
+},
+{
+  code: 2001,
+  description: ":herald:"
+},
+{
+  code: 2002,
+  description: ":guardian:"
+},
+{
+  code: 2003,
+  description: ":crusader:"
+},
+{
+  code: 2004,
+  description: ":archon:"
+},
+{
+  code: 2005,
+  description: ":legend:"
+},
+{
+  code: 2006,
+  description: ":ancient:"
+},
+{
+  code: 2007,
+  description: ":divine:"
+},
+{
+  code: 2008,
+  description: ":immortal:"
+},
+{
+  code: 2009,
+  description: ":top100:"
+},
+{
+  code: 2010,
+  description: ":top1:"
+}
 ]
